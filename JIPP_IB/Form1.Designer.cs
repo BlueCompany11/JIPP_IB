d53b564ff795f987d22e9b133bb8400471157a02
BlueCompany <gronczewskim@gmail.com>
2018-08-23 02:07:25 +0200

    zostalo czyszczenie ekranu

--- JIPP_IB/Form1.Designer.cs
+++ JIPP_IB/Form1.Designer.cs
@@ -28,13 +28,188 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox9, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox8, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox7, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox6, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox5, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox4, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox3, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(20);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1016, 522);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Info;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(332, 167);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.Info;
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox2.Location = new System.Drawing.Point(341, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(332, 167);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.SystemColors.Info;
+            this.pictureBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox3.Location = new System.Drawing.Point(679, 3);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(334, 167);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.SystemColors.Info;
+            this.pictureBox4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox4.Location = new System.Drawing.Point(3, 176);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(332, 167);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.SystemColors.Info;
+            this.pictureBox5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox5.Location = new System.Drawing.Point(341, 176);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(332, 167);
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.SystemColors.Info;
+            this.pictureBox6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox6.Location = new System.Drawing.Point(679, 176);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(334, 167);
+            this.pictureBox6.TabIndex = 5;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Click += new System.EventHandler(this.pictureBox6_Click);
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.BackColor = System.Drawing.SystemColors.Info;
+            this.pictureBox7.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox7.Location = new System.Drawing.Point(3, 349);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(332, 170);
+            this.pictureBox7.TabIndex = 6;
+            this.pictureBox7.TabStop = false;
+            this.pictureBox7.Click += new System.EventHandler(this.pictureBox7_Click);
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.BackColor = System.Drawing.SystemColors.Info;
+            this.pictureBox8.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox8.Location = new System.Drawing.Point(341, 349);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(332, 170);
+            this.pictureBox8.TabIndex = 7;
+            this.pictureBox8.TabStop = false;
+            this.pictureBox8.Click += new System.EventHandler(this.pictureBox8_Click);
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.BackColor = System.Drawing.SystemColors.Info;
+            this.pictureBox9.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox9.Location = new System.Drawing.Point(679, 349);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(334, 170);
+            this.pictureBox9.TabIndex = 8;
+            this.pictureBox9.TabStop = false;
+            this.pictureBox9.Click += new System.EventHandler(this.pictureBox9_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1016, 522);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Kolko i krzyzyk";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- JIPP_IB/Form1.cs
+++ JIPP_IB/Form1.cs
@@ -12,9 +12,177 @@ namespace JIPP_IB
 {
     public partial class Form1 : Form
     {
+        delegate void IsGameOver();
+        event IsGameOver OnIsGameOver;
         public Form1()
         {
             InitializeComponent();
+            currentMove = State.Circle;
+            OnIsGameOver = (() => { MessageBox.Show(currentMove.ToString() + " won"); });
+        }
+        State currentMove;
+        enum State
+        {
+            None,
+            Circle,
+            Cross
+        }
+
+        bool CheckIfGameFinished()
+        {
+            State currentCheckVal = currentMove;
+            //przeszukiwanie poziomo
+            for (int i = 0; i < 3; i++)
+            {
+                bool res = true;
+                for (int j = 0; j < 3; j++)
+                {
+                    if (data[i, j] != currentMove)
+                    {
+                        res = false;
+                        break;
+                    }
+                }
+                if (res == true)
+                    return true;
+            }
+            //przeszuiwanie pionowo
+            for (int i = 0; i < 3; i++)
+            {
+                bool res = true;
+                for (int j = 0; j < 3; j++)
+                {
+                    if (data[j,i] != currentMove)
+                    {
+                        res = false;
+                        break;
+                    }
+                }
+                if (res == true)
+                    return true;
+            }
+            //przeszukiwanie po ukosie
+            bool diagonal1 = true;
+            for (int i = 0; i < 3; i++)
+            {
+                if (data[i,i] != currentMove)
+                {
+                    diagonal1 = false;
+                    break;
+                }
+            }
+            if (diagonal1 == true)
+                return true;
+            //w druga strone
+            bool diagonal2 = true;
+            for (int i = 0; i < 3; i++)
+            {
+                if (data[2-i,2-i] != currentMove)
+                {
+                    diagonal2 = false;
+                    break;
+                }
+            }
+            return diagonal2;
+        }
+
+        //pierwszy element to kolumna, drugi rzad
+        State[,] data = new State[3, 3];
+        void DrawElement(PictureBox pictureBox, int posX, int posY)
+        {
+            if(currentMove == State.Circle)
+            {
+                DrawCircle(pictureBox);
+            }
+            else if(currentMove == State.Cross)
+            {
+                DrawCross(pictureBox);
+            }
+            data[posX, posY] = currentMove;
+            if (CheckIfGameFinished())
+            {
+                OnIsGameOver();
+            }
+            //zamiana wartosc - zawsze na koncu
+            if (currentMove == State.Circle)
+            {
+                currentMove = State.Cross;
+            }
+            else if(currentMove == State.Cross)
+            {
+                currentMove = State.Circle;
+            }
+        }
+        private void DrawCircle(PictureBox pictureBox)
+        {
+            int x = 2;
+            Graphics g = pictureBox.CreateGraphics();
+            int centerX = pictureBox.Width / x;
+            int centerY = pictureBox.Height / x;
+            int radius = pictureBox.Height / x;
+            g.DrawEllipse(Pens.Red, centerX - radius, centerY - radius,
+                      radius + radius, radius + radius);
+            
+        }
+        private void DrawCross(PictureBox pictureBox)
+        {
+            Graphics g = pictureBox.CreateGraphics();
+            g.DrawLine(new Pen(Color.Black, 1),
+                     1,
+                     1,
+                     pictureBox.Width,
+                     pictureBox.Height);
+
+            g.DrawLine(new Pen(Color.Black, 1),
+                     pictureBox.Width,
+                     1,
+                     1,
+                     pictureBox.Height);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            DrawElement(pictureBox1,0,0);
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            DrawElement(pictureBox2, 1, 0);
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            DrawElement(pictureBox3, 2, 0);
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            DrawElement(pictureBox4, 0, 1);
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            DrawElement(pictureBox5, 1, 1);
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+            DrawElement(pictureBox6, 2, 1);
+        }
+
+        private void pictureBox7_Click(object sender, EventArgs e)
+        {
+            DrawElement(pictureBox7, 0, 2);
+        }
+
+        private void pictureBox8_Click(object sender, EventArgs e)
+        {
+            DrawElement(pictureBox8, 1, 2);
+        }
+
+        private void pictureBox9_Click(object sender, EventArgs e)
+        {
+            DrawElement(pictureBox9, 2, 2);
         }
     }
 }
